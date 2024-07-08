17c47535a0a96c28436d0cda915b812a2c2d54ba
Boyar <boiarnikov.ia@phystech.edu>
2024-07-08 21:52:06 +0300

    Add unittests

--- encoders/__init__.py
+++ encoders/__init__.py
@@ -1,5 +1,3 @@
 from .caesar import Caesar
 from .morse import Morse
 from .vigenere import Vigenere
-
-print("encoders are ready")
--- encoders/vigenere.py
+++ encoders/vigenere.py
@@ -7,6 +7,9 @@ class Vigenere(BaseEncoder):
     def key(self, new_key: str):
         if not isinstance(new_key, str) or len(new_key) == 0:
             raise AttributeError(f'Ключом может быть только непустая строка, но получен "{new_key}"')
+        for ch in new_key:
+            if get_index(ch) == -1:
+                raise AttributeError(f'Символ "{ch}" не индексируем')
         self._key = new_key
 
     def encode(self, text: str) -> str:
