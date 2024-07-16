cab94dd76892ca0ac73a531a053f0bc6f2456df0
yalaik <oduur1@ya.ru>
2024-07-17 00:40:42 +0500

    fixed atbash.py

--- encoders/atbash.py
+++ encoders/atbash.py
@@ -14,15 +14,14 @@ class Atbash(BaseEncoder):
     def _atbash(self, text: str) -> str:
         result = []
         for char in text:
-            if char.isalpha():
-                if 'a' <= char <= 'z':  # English lowercase
-                    result.append(chr(219 - ord(char)))  # 'a' + 'z' = 219
-                elif 'A' <= char <= 'Z':  # English uppercase
-                    result.append(chr(155 - ord(char)))  # 'A' + 'Z' = 155
-                elif 'а' <= char <= 'я':  # Russian lowercase
-                    result.append(chr(224 + (ord('я') - ord(char))))  # 'а' + 'я' = 224 + (я - char)
-                elif 'А' <= char <= 'Я':  # Russian uppercase
-                    result.append(chr(192 + (ord('Я') - ord(char))))  # 'А' + 'Я' = 192 + (Я - char)
+            if 'a' <= char <= 'z':  # English lowercase
+                result.append(chr(219 - ord(char)))  # 'a' + 'z' = 219
+            elif 'A' <= char <= 'Z':  # English uppercase
+                result.append(chr(155 - ord(char)))  # 'A' + 'Z' = 155
+            elif 'а' <= char <= 'я':  # Russian lowercase
+                result.append(chr(ord('я') + ord('а') - ord(char)))  # Reverse Russian lowercase
+            elif 'А' <= char <= 'Я':  # Russian uppercase
+                result.append(chr(ord('Я') + ord('А') - ord(char)))  # Reverse Russian uppercase
             else:
-                result.append(char)
+                result.append(char)  # Non-alphabetical characters remain unchanged
         return ''.join(result)
--- main.py
+++ main.py
@@ -7,15 +7,15 @@ import encoders
 v=encoders.atbash.Atbash()
 v.encode_to_file(input(), "MY_SECRET.txt")
 print(v.decode_from_file("MY_SECRET.txt"))
-
-try:
-    1/int(input())
-    print("Поделилось!")
-except ZeroDivisionError:
-    print("Кто-то поделил на ноль!!")
-except ValueError:
-    print("Не получилось превратить в число!")
-else:
-    print("Как здорово что ничего не сломалось!")
-finally:
-    print("А теперь я спать")
+#
+# try:
+#     1/int(input())
+#     print("Поделилось!")
+# except ZeroDivisionError:
+#     print("Кто-то поделил на ноль!!")
+# except ValueError:
+#     print("Не получилось превратить в число!")
+# else:
+#     print("Как здорово что ничего не сломалось!")
+# finally:
+#     print("А теперь я спать")
